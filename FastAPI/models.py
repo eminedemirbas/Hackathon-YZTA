52a2f965d3d13041cf4e87690b5c545ec47f2563
Sude Özübek <130099053+Sudeozubek@users.noreply.github.com>
2025-05-05 23:31:07 +0300

    Update models.py

--- FastAPI/models.py
+++ FastAPI/models.py
@@ -2,14 +2,14 @@ from pydantic import BaseModel, Field
 
 class UserData(BaseModel):
     car_km: float
-    meat_days: int
+    daily_water_use: float
     plastic_use: bool
 
     class Config:
         json_schema_extra = {
             'example': {
                 'car_km': 15.0,
-                'meat_days': 2,
+                'daily_water_use': 2,
                 'plastic_use': False
             }
         }
@@ -18,6 +18,6 @@ def get_rozet(score: float) -> dict:
     if score < 50:
         return {"title": "Çevreye Zarar Veren", "emoji": "🥀"}
     elif score < 80:
-        return {"title": "Yeşeren Yolcu", "emoji": "🌱"}
+        return {"title": "Yeşeren Yolcu", "emoji": "🌿"}
     else:
-        return {"title": "Gezegen Koruyucusu", "emoji": "🌳"}
+        return {"title": "Gezegen Koruyucusu", "emoji": "🌸"}

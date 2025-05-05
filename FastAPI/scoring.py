108776f5135ea72025aa0fbded31c4a5eb03354a
Sude Özübek <130099053+Sudeozubek@users.noreply.github.com>
2025-05-05 23:31:25 +0300

    Update scoring.py

--- FastAPI/scoring.py
+++ FastAPI/scoring.py
@@ -1,7 +1,7 @@
-def calculate_score(car_km, meat_days, plastic_use):
+def calculate_score(car_km, daily_water_use, plastic_use):
     score = 100
     score -= car_km * 0.3
-    score -= meat_days * 5
+    score -= daily_water_use * 0.2
     if plastic_use:
         score -= 10
     return max(0, min(score, 100))

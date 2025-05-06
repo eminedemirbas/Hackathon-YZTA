7236158cf005f9ebecb601b1020fb05c728a7750
AysenurTuran <120721647+AysenurTuran@users.noreply.github.com>
2025-05-06 19:53:59 +0300

    Add Chatbot structure

---
+++ static/jss/script.js
@@ -0,0 +1,8 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+  const toggleButton = document.querySelector('.chatbot-toggle');
+  const chatbotWindow = document.querySelector('.chatbot-window');
+
+  toggleButton.addEventListener('click', function () {
+    chatbotWindow.classList.toggle('active');
+  });
+});
